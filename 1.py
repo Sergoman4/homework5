7969c00cbabe761722eea74d8521094d7fabe4e8
Sergoman4 <28062002sergey@gmail.com>
2024-06-12 14:24:06 +1000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+immutable_var = 1,['привет'],1.5
+print(immutable_var)
+immutable_var[1][0] = 23
+print(immutable_var)
+mutable_list = ['pyton',True,2]
+print(mutable_list)
+mutable_list[0]=2
+print(mutable_list)
